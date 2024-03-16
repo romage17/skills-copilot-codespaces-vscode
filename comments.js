b58d90f7b6576e36fee3ffe51ac5e87ebce18c82
romage17 <19826898+romage17@users.noreply.github.com>
2024-03-16 22:44:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,35 @@
+// create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/comments', function(req, res) {
+    fs.readFile(commentsPath, function(err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        var comments = JSON.parse(data);
+        var newComment = {
+            id: Date.now()
+        
+        };
+    });
+});
